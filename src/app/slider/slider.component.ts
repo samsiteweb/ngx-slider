f5d19985627681910dc885a4df233af01290a0fd
lawal samuel <samthedonz@gmail.com>
2020-02-17 00:12:26 -0800

    improved

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,107 +1,112 @@
-import { Component, OnInit, Input, HostListener, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, HostListener, ElementRef, OnDestroy } from '@angular/core';
 import { Slider } from './class/slider';
 import { ISliderConfig } from './interface/slider-config';
 import { ISlide } from './interface/slide';
 
 @Component({
-  // tslint:disable-next-line:component-selector
-  selector: 'ngx-slider',
-  templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.css']
+	// tslint:disable-next-line:component-selector
+	selector: 'ngx-slider',
+	templateUrl: './slider.component.html',
+	styleUrls: [ './slider.component.css' ]
 })
 export class SliderComponent implements OnInit {
+	@Input() init: Slider;
 
-  @Input() init: Slider;
+	public sliderItems: Array<ISlide>;
+	public config: ISliderConfig;
+	public componentWidth: number;
+	public currentItemIndex: number;
+	public activeTitle: string;
+	public sliderStyle: any;
+	public slideStyle: any;
+	public interval: any;
+	constructor(public el: ElementRef) {
+		this.currentItemIndex = 0;
+		this.activeTitle = '';
+	}
 
-  public sliderItems: Array<ISlide>;
-  public config: ISliderConfig;
-  public componentWidth: number;
-  public currentItemIndex: number;
-  public activeTitle: string;
-  public sliderStyle: any;
-  public slideStyle: any;
+	ngOnInit() {
+		this.sliderItems = this.init.items;
+		this.config = this.init.config;
+		this.setTitle();
+		this.resize();
+		this.setSliderStyle();
+		this.interval = setInterval(() => {
+			this.goTo('next');
+		}, 10000);
+	}
 
-  constructor(
-    public el: ElementRef
-  ) {
-    this.currentItemIndex = 0;
-    this.activeTitle = '';
-  }
+	@HostListener('window:resize', [ '$event.target' ])
+	onResize() {
+		this.resize();
+	}
 
-  ngOnInit() {
-    this.sliderItems = this.init.items;
-    this.config = this.init.config;
-    this.setTitle();
-    this.resize();
-    this.setSliderStyle();
-  }
+	private resize() {
+		this.componentWidth =
+			this.el.nativeElement.parentElement.clientWidth - this.getPreviewWidth() * this.getNumberOfPreview();
+		this.setSlideStyle();
+	}
 
-  @HostListener('window:resize', ['$event.target'])
-  onResize() {
-    this.resize();
-  }
+	private setSliderStyle(): void {
+		const width = `${this.getWrapperWidth()}px`;
+		const left = this.getWrapperLeft() ? `-${this.getWrapperLeft() - this.getPreviewWidth()}px` : 0;
 
-  private resize() {
-    this.componentWidth = this.el.nativeElement.parentElement.clientWidth - (this.getPreviewWidth() * this.getNumberOfPreview());
-    this.setSlideStyle();
-  }
+		this.sliderStyle = {
+			width: width,
+			left: left,
+			transition: `next ${this.config.transitionDuration}s`
+		};
+	}
 
-  private setSliderStyle(): void {
-    const width = `${this.getWrapperWidth()}px`;
-    const left = this.getWrapperLeft() ? `-${this.getWrapperLeft() - this.getPreviewWidth()}px` : 0;
+	private setSlideStyle(): void {
+		this.slideStyle = {
+			width: `${this.componentWidth}px`
+		};
+	}
 
-    this.sliderStyle = {
-      width: width,
-      left: left,
-      transition: `left ${this.config.transitionDuration}s`
-    };
-  }
+	private setTitle(): void {
+		this.activeTitle = this.sliderItems.length ? this.sliderItems[this.currentItemIndex].title : '';
+	}
 
-  private setSlideStyle(): void {
-    this.slideStyle = {
-      width: `${this.componentWidth}px`
-    };
-  }
+	private getNumberOfPreview(): number {
+		return this.config.showPreview ? this.config.numberOfPreview : 0;
+	}
 
-  private setTitle(): void {
-    this.activeTitle =
-      this.sliderItems.length ? this.sliderItems[this.currentItemIndex].title : '';
-  }
+	private getPreviewWidth(): number {
+		return this.config.showPreview ? this.config.previewWidth : 0;
+	}
 
-  private getNumberOfPreview(): number {
-    return this.config.showPreview ? this.config.numberOfPreview : 0;
-  }
+	private getWrapperWidth(): number {
+		return this.sliderItems.length ? this.sliderItems.length * this.componentWidth : this.componentWidth;
+	}
 
-  private getPreviewWidth(): number {
-    return this.config.showPreview ? this.config.previewWidth : 0;
-  }
+	private getWrapperLeft(): number {
+		return this.currentItemIndex * this.componentWidth;
+	}
 
-  private getWrapperWidth(): number {
-    return this.sliderItems.length ? this.sliderItems.length * this.componentWidth : this.componentWidth;
-  }
+	private goTo(action: any): void {
+		if (action === 'next') {
+			if (this.sliderItems.length - 1 > this.currentItemIndex) {
+				this.currentItemIndex++;
+			} else {
+				this.currentItemIndex = this.config.loop ? 0 : this.currentItemIndex;
+			}
+		} else if (action === 'previous') {
+			if (this.currentItemIndex > 0) {
+				this.currentItemIndex--;
+			} else {
+				this.currentItemIndex = this.config.loop ? this.sliderItems.length - 1 : this.currentItemIndex;
+			}
+		} else {
+			this.currentItemIndex = action;
+		}
+		this.setSliderStyle();
+		this.setTitle();
+	}
 
-  private getWrapperLeft(): number {
-    return this.currentItemIndex * this.componentWidth;
-  }
-
-  private goTo(action: any): void {
-    if (action === 'next') {
-      if (this.sliderItems.length - 1 > this.currentItemIndex) {
-        this.currentItemIndex++;
-      } else {
-        this.currentItemIndex = this.config.loop ? 0 : this.currentItemIndex;
-      }
-    } else if (action === 'previous') {
-      if (this.currentItemIndex > 0) {
-        this.currentItemIndex--;
-      } else {
-        this.currentItemIndex = this.config.loop ? this.sliderItems.length - 1 : this.currentItemIndex;
-      }
-    } else {
-      this.currentItemIndex = action;
-    }
-    this.setSliderStyle();
-    this.setTitle();
-  }
-
+	ngOnDestroy() {
+		if (this.interval) {
+			clearInterval(this.interval);
+		}
+	}
 }
